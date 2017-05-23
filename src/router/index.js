6bab939c8b692b6a1bf15ccea5a14ef583d0ac64
unknown <1335674452@qq.com>
2017-05-23 23:00:50 +0800

    增加spinner

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import FastClick from 'fastclick'
+
+//fastclick
+document.addEventListener('DOMContentLoaded', function(){
+	if(FastClick ) FastClick.attach(document.body);
+},false);
+
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+	base: __dirname,
+	routes: [
+	    {
+	      path: '/',
+	      name: 'Hello',
+	      component: Hello
+	    }
+	]
 })
